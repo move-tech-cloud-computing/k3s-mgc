f90dfb88f9e1b67902a3c057ec21d13cc3cb4201
Danilo Andrade <danh.andrade@outlook.com>
2026-07-29 19:18:18 -0300

    chore: remove comentários inline do cabeçalho (diagnose/fix)

--- k3s.sh
+++ k3s.sh
@@ -11,10 +11,10 @@
 #   ~/k3s.sh kubernetes cluster get                 --cluster-id ID
 #   ~/k3s.sh kubernetes cluster delete              --cluster-id ID
 #   ~/k3s.sh kubernetes cluster configure-registry  --cluster-id ID
-#   ~/k3s.sh kubernetes cluster diagnose                           # diagnóstico de todos os clusters
-#   ~/k3s.sh kubernetes cluster diagnose            --cluster-id ID  # diagnóstico de um cluster específico
-#   ~/k3s.sh kubernetes cluster fix                                # recupera acesso em todos os clusters com problema
-#   ~/k3s.sh kubernetes cluster fix                 --cluster-id ID  # recupera acesso em um cluster específico
+#   ~/k3s.sh kubernetes cluster diagnose
+#   ~/k3s.sh kubernetes cluster diagnose            --cluster-id ID
+#   ~/k3s.sh kubernetes cluster fix
+#   ~/k3s.sh kubernetes cluster fix                 --cluster-id ID
 #
 #   ~/k3s.sh network ip-cleanup   — lista e remove IPs públicos órfãos
 
